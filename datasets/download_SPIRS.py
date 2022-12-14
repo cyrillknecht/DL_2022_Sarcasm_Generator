5114a1445fee69f3282ed184ef171aff32df501c
dave-95 <davzehnd@student.ethz.ch>
2022-12-14 13:52:52 +0100

    Update Dataset
    Dataset with context, remove iSarcasm

--- datasets/download_SPIRS.py
+++ datasets/download_SPIRS.py
@@ -1,9 +1,22 @@
 from auth import *
 import csv
 from tqdm import tqdm
+import json
 
 
-def get_dataset(file, label, id_idx):
+def get_tweet(tweet_id):
+    try:
+        tweet = api.get_status(id=tweet_id)
+        # Remove line breaks
+        return tweet.text.replace('\n', ' ').replace('\r', '')
+    except:
+        pass
+
+    return ''
+
+
+def get_dataset(file, label, eli_idx, sar_idx):
+    print("")
     print("Downloading SPIRS/"+file+".csv")
     with open("SPIRS/"+file+".csv") as csv_file:
         lines = len(csv_file.readlines())
@@ -12,21 +25,14 @@ def get_dataset(file, label, id_idx):
         with open("SPIRS/"+file+".csv") as csvFile:
             reader = csv.reader(csvFile)
             for row in tqdm(reader, total=lines):
-                tweet_id = row[id_idx]
-                try:
-                    tweet = api.get_status(id=tweet_id)
-                    # Remove line breaks
-                    text = tweet.text.replace('\n', ' ').replace('\r', '')
-                    # Escape double quotes
-                    text = text.replace('"', '\\"')
-                    # Escape backslash
-                    text = text.replace('\\', '\\\\')
+                eli_text = get_tweet(row[eli_idx])
+                sar_text = get_tweet(row[sar_idx])
 
-                    jsonFile.write('{"label":"' + label + '","text":"' + text + '"}\n')
+                if sar_text and eli_text:
 
-                except:
-                    pass
+                    json.dump({"label": label, "context": eli_text, "text": sar_text}, jsonFile)
+                    jsonFile.write('\n')
 
 
-get_dataset('SPIRS-sarcastic-ids', 'SARCASM', 4)
-get_dataset('SPIRS-non-sarcastic-ids', 'NOT_SARCASM', 2)
+get_dataset('SPIRS-sarcastic-ids', 'SARCASM', 6, 4)
+get_dataset('SPIRS-non-sarcastic-ids', 'NOT_SARCASM', 3, 1)
--- datasets/preprocess_datasets.py
+++ datasets/preprocess_datasets.py
@@ -7,25 +7,25 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
-def preprocess(folder, readFile, saveFile):
+def preprocess(folder, readFile, saveFile, field):
     df = pd.read_json(folder+'/'+readFile, lines=True)
     # Remove @...
-    df['text'] = df['text'].str.replace('(@\w+)', '')
+    df[field] = df[field].replace(regex=['(@\w+)'], value='')
     # Remove links
-    df['text'] = df['text'].replace(regex=['(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)'], value='')
+    df[field] = df[field].replace(regex=['(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)'], value='')
     # Remove multiple spaces
-    df['text'] = df['text'].replace(regex=['  +'], value=' ')
+    df[field] = df[field].replace(regex=['  +'], value=' ')
     # Remove emojis
-    df['text'] = df['text'].apply(lambda s: ''.join(filter(lambda c: ord(c) < 256, s)))
+    df[field] = df[field].apply(lambda s: ''.join(filter(lambda c: ord(c) < 256, s)))
     # Remove leading and trailing spaces
-    df['text'] = df['text'].str.strip()
+    df[field] = df[field].str.strip()
     # Drop empty columns
-    df['text'].replace('', np.nan, inplace=True)
-    df.dropna(subset=['text'], inplace=True)
+    df[field].replace('', np.nan, inplace=True)
+    df.dropna(subset=[field], inplace=True)
     # Save
     save_as_json(df, folder+'/'+saveFile)
 
 
-preprocess('ETS-Sarcasm', 'ETS.jsonl', 'ETS_preprocessed.jsonl')
-preprocess('iSarcasm', 'iSarcasm.jsonl', 'iSarcasm_preprocessed.jsonl')
-preprocess('SPIRS', 'SPIRS.jsonl', 'SPIRS_preprocessed.jsonl')
+preprocess('ETS-Sarcasm', 'ETS.jsonl', 'ETS_preprocessed.jsonl', 'text')
+preprocess('SPIRS', 'SPIRS.jsonl', 'SPIRS_preprocessed.jsonl', 'context')
+preprocess('SPIRS', 'SPIRS.jsonl', 'SPIRS_preprocessed.jsonl', 'text')
