8920a097bebafc42f7fc45726b78db2b9923d23a
njoel-ethz <49750403+njoel-ethz@users.noreply.github.com>
2023-01-02 12:25:52 +0100

    Added eda filenames

--- judge.py
+++ judge.py
@@ -4,9 +4,15 @@
 from classifier import Classifier
 import csv
 
-TRAINED_CLASSIFIER_PATH = 'models/judge'
-FOLDER = 'generated'
-FILES = ['eda_augmented_results.csv']
+TRAINED_CLASSIFIER_PATH = "models/judge"
+FOLDER = "generated"
+
+FILES = [
+    "eda_augmented_results_30_10.csv",
+    "eda_augmented_results_20_20.csv",
+    "eda_augmented_results_10_30.csv",
+    "eda_augmented_results_0_40.csv",
+]
 
 judge = Classifier()
 judge.load_classifier(TRAINED_CLASSIFIER_PATH)
@@ -15,23 +21,19 @@ for file in FILES:
     samples = 0
     sarcasm = 0
     if file[-4:] == ".txt":
-        with open(FOLDER+"/"+file) as f:
+        with open(FOLDER + "/" + file) as f:
             for line in f:
                 samples += 1
                 if judge.classify_tweet(line):
                     sarcasm += 1
-                if samples % 10 == 0:
-                    print(f"Current sample: {samples}")
-    elif file[-4:] == ".csv": # adapted from gpt2simple's load_dataset
-         with open(FOLDER+"/"+file, 'r', encoding='utf8', errors='ignore') as f:
-                f.readline()   # skip header
-                reader = csv.reader(f)
-                for line in reader:
-                    samples += 1
-                    if judge.classify_tweet(line[0]):
-                        sarcasm += 1
-                    if samples % 10 == 0:
-                        print(f"Current sample: {samples}")
+    elif file[-4:] == ".csv":  # adapted from gpt2simple's load_dataset
+        with open(FOLDER + "/" + file, "r", encoding="utf8", errors="ignore") as f:
+            f.readline()  # skip header
+            reader = csv.reader(f)
+            for line in reader:
+                samples += 1
+                if judge.classify_tweet(line[0]):
+                    sarcasm += 1
 
     print("Results for " + file)
     print("Total Samples: " + str(samples))
